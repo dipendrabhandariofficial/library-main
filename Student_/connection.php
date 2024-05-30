48a86fbf6a7dc9fdb83471c82538a9c9c0ce2956
Nischal Nepal <134925151+Nishchal777@users.noreply.github.com>
2024-05-30 15:31:15 +0545

    Update connection.php

--- Student_/connection.php
+++ Student_/connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$db=mysqli_connect("localhost","root","","library-main");  
+	$db=mysqli_connect("localhost","root","","library");  
 					/* server name, username, passwor, database name */
 
 ?>
